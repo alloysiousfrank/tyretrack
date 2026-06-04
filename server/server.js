c2d94823add5da0d6534f4b4afe365ce5c61df5d
Alloysious Frank <aloysiusfrank20@gmail.com>
2026-06-04 21:37:11 +0530

    Fixed CORS

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://tyretrackfrontend.vercel.app",
+      "https://tyretrack-neon.vercel.app",
     ],
     credentials: true,
   })
